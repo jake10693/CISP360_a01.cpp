75065b4d48f0223e672ab22a622f7e9bb7e02589
Jacob Carver <55562256+jake10693@users.noreply.github.com>
2020-10-16 23:03:01 -0700

    First commit - Added a01.cpp file as assignment instructed - Created comment section above to define owner of the program - No line should be longer than 80 characters

---
+++ a01.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+** Program Name:    Hello World
+** File Name:       a01.cpp
+** Author:          Jacob Carver
+** Date:            October 16, 2020
+** Assignment:      01
+** Description:     First program, prints "Hello World!"
+** Sources:         none.
+******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
